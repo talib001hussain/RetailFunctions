8ad13ea604595a009a19f9757afe50c9de3a8436
CONFIZ\talib.hussain <talib.hussain@confiz.com>
2022-10-05 12:22:29 +0500

    created product service files

---
+++ src/main/java/controller/ProductController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import dto.ProductRequest;
+import dto.ProductResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import service.ProductService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+}
